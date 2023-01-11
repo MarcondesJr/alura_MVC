d7a079e6b9e99628b131700e094b77ed018c9e99
MarcondesJr <marcondestecinfo@gmail.com>
2023-01-11 16:53:50 -0400

    Treinamento MVC - Segurança e API

--- config/routes.php
+++ config/routes.php
@@ -6,5 +6,7 @@ return [
     'POST|/novo-video' => \Alura\Mvc\Controller\NewVideoController::class,
     'GET|/editar-video' => \Alura\Mvc\Controller\VideoFormController::class,
     'POST|/editar-video' => \Alura\Mvc\Controller\EditVideoController::class,
-    'GET|/remover-video' => \Alura\Mvc\Controller\DeleteVideoController::class
+    'GET|/remover-video' => \Alura\Mvc\Controller\DeleteVideoController::class,
+    'GET|/login' => \Alura\Mvc\Controller\LoginFormController::class,
+    'POST|/login' => \Alura\Mvc\Controller\LoginController::class
 ];
